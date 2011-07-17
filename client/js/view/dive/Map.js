633d763f3b44fe043a38d847f2631fbae8ff7ffb
goldledoigt <goldledoigt@chewam.com>
2011-07-17 04:35:47 +0200

    disable map

--- client/js/view/dive/Map.js
+++ client/js/view/dive/Map.js
@@ -13,38 +13,38 @@ Ext.define('CB.view.dive.Map', {
 
     initComponent : function() {
 
-        this.items = [{
-            border: false,
-            xtype: 'gmappanel',
-            zoomLevel: 14,
-            gmapType: 'map',
-            mapConfOpts: ['enableScrollWheelZoom','enableDoubleClickZoom','enableDragging'],
-            mapControls: ['GSmallMapControl','GMapTypeControl','NonExistantControl'],
-            setCenter: {
-                geoCodeAddr: '4 Yawkey Way, Boston, MA, 02215-3409, USA',
-                marker: {title: 'Fenway Park'}
-            },
-            markers: [{
-                lat: 42.339641,
-                lng: -71.094224,
-                marker: {title: 'Boston Museum of Fine Arts'},
-                listeners: {
-                    click: function(e){
-                        Ext.Msg.alert({title: 'Its fine', text: 'and its art.'});
-                    }
-                }
-            },{
-                lat: 42.339419,
-                lng: -71.09077,
-                marker: {title: 'Northeastern University'}
-            }]
-        }];
+        // this.items = [{
+        //     border: false,
+        //     xtype: 'gmappanel',
+        //     zoomLevel: 14,
+        //     gmapType: 'map',
+        //     mapConfOpts: ['enableScrollWheelZoom','enableDoubleClickZoom','enableDragging'],
+        //     mapControls: ['GSmallMapControl','GMapTypeControl','NonExistantControl'],
+        //     setCenter: {
+        //         geoCodeAddr: '4 Yawkey Way, Boston, MA, 02215-3409, USA',
+        //         marker: {title: 'Fenway Park'}
+        //     },
+        //     markers: [{
+        //         lat: 42.339641,
+        //         lng: -71.094224,
+        //         marker: {title: 'Boston Museum of Fine Arts'},
+        //         listeners: {
+        //             click: function(e){
+        //                 Ext.Msg.alert({title: 'Its fine', text: 'and its art.'});
+        //             }
+        //         }
+        //     },{
+        //         lat: 42.339419,
+        //         lng: -71.09077,
+        //         marker: {title: 'Northeastern University'}
+        //     }]
+        // }];
 
         this.callParent(arguments);
     },
 
     setCenter: function(address) {
-        var map = this.down('gmappanel').geoCodeLookup(address);
+        // var map = this.down('gmappanel').geoCodeLookup(address);
     }
  
 });
